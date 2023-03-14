dbeb1fdcbdee50b2db72bed7e7359a0e2ed85971
鑫 杨 <1766080953@qq.com>
2023-03-14 21:55:41 +0800

    第三次作业

---
+++ Assignment3/Shape/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Shape
+{
+    public abstract class Shape
+    {
+        public abstract int getArea();
+        public abstract bool isLeagle();
+    }
+    public class Rectangle:Shape
+    {
+        protected int width, height;
+        public void setWidth(int width)
+        {
+            this.width = width;
+        }
+        public void setHeight(int height)
+        {
+            this.height = height;
+        }
+        public override int getArea()
+        {
+            return this.width * this.height;
+        }
+        public override bool isLeagle()
+        {
+            if (this.width == this.height || width < 0 || height < 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+    public class Square : Rectangle
+    {
+        public void setHeight()
+        {
+            this.height = this.width;
+        }
+        public new bool isLeagle()
+        {
+            if (this.width == this.height && width > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    public class Triangle:Shape
+    {
+        int a, b, c;
+        public void setLength(int a, int b, int c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public override int getArea()
+        {
+            int p = (a + b + c) / 2;
+            return Convert.ToInt32(Math.Sqrt(p * (p - a) * (p - b) * (p - c)));
+        }
+        public override bool isLeagle()
+        {
+            if (a + b > c && a - b < c)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int shapety;
+            int height, width;
+            int a, b, c;
+            Console.WriteLine("请输入形状：1.矩形 2.正方形 3.三角形");
+            shapety = Convert.ToInt32(Console.ReadLine());
+            if(shapety == 1)
+            {
+                Rectangle p = new Rectangle();
+                Console.WriteLine("请输入长和宽");
+                height = Convert.ToInt32(Console.ReadLine());
+                width = Convert.ToInt32(Console.ReadLine());
+                p.setWidth(width);
+                p.setHeight(height);
+                if(!p.isLeagle())
+                {
+                    Console.WriteLine("非法的图形");
+                    return;
+                }
+                Console.WriteLine("面积为：" + p.getArea());
+            }
+            else if(shapety == 2)
+            {
+                Square s = new Square();
+                Console.WriteLine("请输入边长");
+                width = Convert.ToInt32(Console.ReadLine());
+                s.setWidth(width);
+                s.setHeight();
+                if(!s.isLeagle())
+                {
+                    Console.WriteLine("非法的图形");
+                    return;
+                }
+                Console.WriteLine("面积为：" +s.getArea());
+            }
+            else if(shapety == 3)
+            {
+                Triangle t = new Triangle();
+                Console.WriteLine("请输入三边：");
+                a = Convert.ToInt32(Console.ReadLine());
+                b = Convert.ToInt32(Console.ReadLine());
+                c = Convert.ToInt32(Console.ReadLine());
+                t.setLength(a, b, c);
+                if(!t.isLeagle() )
+                {
+                    Console.WriteLine("非法的图形");
+                    return;
+                }
+                Console.WriteLine("面积为：" + t.getArea());
+            }
+        }
+    }
+}
---
+++ Assignment3/ShapeFactory/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeFactory
+{
+    public abstract class Shape
+    {
+        public abstract int getArea();
+    }
+    public class Rectangle : Shape
+    {
+        protected int width, height;
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public override int getArea()
+        {
+            return this.width * this.height;
+        }
+    }
+    public class Square : Shape
+    {
+        private int width, height;
+        public Square(int width)
+        {
+            this.width = width;
+            this.height = this.width;
+        }
+       
+        public override int getArea()
+        {
+            return this.width * this.height;
+        }
+    }
+    public class Triangle : Shape
+    {
+        int a, b, c;
+        public Triangle(int a, int b, int c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public override int getArea()
+        {
+            int p = (a + b + c) / 2;
+            return Convert.ToInt32(Math.Sqrt(p * (p - a) * (p - b) * (p - c)));
+        }
+    }
+    public static class ShapeFactory
+    {
+        public static Shape CreateShape(int type)
+        {
+            Random r = new Random();
+            switch (type)
+            {
+                case 1:
+                    return new Square(r.Next(1, 10));
+                case 2:
+                    return new Rectangle(r.Next(1, 10), r.Next(1, 10));
+                case 3:
+                    return new Triangle(r.Next(1, 10), r.Next(1, 10), r.Next(1, 10));
+                default:
+                    throw new ArgumentException("错误的类型");
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double totalArea = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                totalArea += ShapeFactory.CreateShape(new int[] { 1, 2, 3 }[new Random().Next(3)]).getArea();
+            }
+            Console.WriteLine("总面积为: " + totalArea);
+        }
+    }
+}
